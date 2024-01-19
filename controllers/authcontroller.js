2c0b054856b36e113b24833db316ecf83cb5d9e2
Mukhtar <mukhtarabdulfatai@gmail.com>
2024-01-19 14:56:50 +0100

    error

--- controllers/authcontroller.js
+++ controllers/authcontroller.js
@@ -65,7 +65,7 @@ export const signUp = async (req, res) => {
             }
         });
     } catch (error) {
-        console.log(console.log(error))
+        console.log(error)
         return res.status(500).json({
             status: false,
             message: "An error occurred" + error,
